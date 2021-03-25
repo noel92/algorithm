9abc1663619c1d37c48b90e76aa387fce17762e3
wangjing <wangjing0313@hupu.com>
2021-03-25 10:19:14 +0800

    2021-03-25 commit

--- src/linked_list/singly_linked_list.c
+++ src/linked_list/singly_linked_list.c
@@ -2,9 +2,5 @@
 // Created by noel on 2021/3/23.
 //
 
+#include <stdlib.h>
 #include "singly_linked_list.h"
-
-typedef struct _Node {
-    int val;
-    _Node* next;
-}* Node;
--- src/linked_list/singly_linked_list.h
+++ src/linked_list/singly_linked_list.h
@@ -5,4 +5,28 @@
 #ifndef ALGORITHM_SINGLY_LINKED_LIST_H
 #define ALGORITHM_SINGLY_LINKED_LIST_H
 
+typedef struct SinglyLinkedListNode {
+    void *val;
+    SinglyLinkedListNode *next;
+} SinglyLinkedListNode;
+
+typedef SinglyLinkedListNode SLLNode;
+
+typedef struct SinglyLinkedList {
+    SLLNode *head;
+    int length;
+} SinglyLinkedList;
+
+typedef SinglyLinkedList SLL;
+
+SLL *create_singly_linked_list();
+
+int append_node(SLL *list, void *val);
+
+int remove_node(SLL *list, int index);
+
+int update_node(SLL *list, int index, void *val);
+
+SLLNode *find_node(SLL *list, int index);
+
 #endif //ALGORITHM_SINGLY_LINKED_LIST_H
